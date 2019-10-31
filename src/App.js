1dd2b1f65d170227747562cd556f6b1d0d27362d
Gaston Ndanyuzwe <ndanyuzwe@hotmail.com>
2019-10-31 19:43:26 +0100

    add styled-components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const width = 10;
+const Wrapper = styled.div`
+border: ${({color}) => color} dashed ${width}px;
+height: calc(100% - ${width * 2}px);
+width: calc(100% - ${width * 2}px);
+transition: border 300ms ease-in-out;
+display: flex;
+justify-content: center;
+align-items: center;
+
+span {
+    font-size: 3em;
+    color: ${props => props.color};
+}
+`;
+
+class App extends React.Component {
+  
+    state = {
+        color: 'orangeyellow',
+        num: 0
+    }
+
+    intid = null;
+
+    componentDidMount() {
+        const colors = ['deepskyblue', 'springgreen', 'darkred', 'salmon', 'indianred', 'purple', 'orange', 'violet'];
+        this.intid = setInterval(() => {
+            const index = Math.floor(Math.random() *8);
+            this.setState({num: index});
+            const color = colors[index];
+            this.setState({color});
+        }, 1000);
+    }
+
+    componentWillUnmount() {
+        if(this.intid) {
+            clearInterval(this.intid)
+        }
+    }
+    render() {
+        return (
+            <Wrapper color={this.state.color}>
+                <span>{this.state.num}</span>
+                </Wrapper>
+        );
+    }
 }
 
 export default App;
